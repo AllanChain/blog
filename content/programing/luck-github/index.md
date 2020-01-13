ca3ca189dc9b1cae74bde1d5deec2927799735a6
Allan Chain <allanchain@pku.edu.cn>
2020-01-13 15:54:58 +0800

    water

---
+++ content/programing/luck-github/index.md
@@ -0,0 +1,17 @@
+---
+Title: "Lucky GitHub Number"
+Date: 2020-01-13T15:47:34+08:00
+Author: Allan Chain
+Categories:
+    - Daily
+Tags: 
+    - Contribution
+Description: >
+    Record the lucky GitHub contribution number.
+---
+
+![shot](screenshot.png)
+
+1234! That's cool!
+
+You can easily notice June the separator.
--- new.cmd
+++ new.cmd
@@ -10,15 +10,14 @@ if %blog_type%==1 set blog_folder="academy"
 if not defined blog_folder goto ask_type
 
 set /p blog_name="blog post file name: "
-set /p create_folder="need a folder? Non-empty means no: "
+set /p create_folder="need a folder? Empty means no: "
 
 if "%create_folder%" == "" (
     hugo new %blog_folder%/%blog_name%.md
     explorer content\%blog_folder%\%blog_name%.md
-    echo content\%blog_folder%\%blog_name%.md
 ) else (
     hugo new %blog_folder%/%blog_name%/index.md
+    explorer content\%blog_folder%\%blog_name%
     explorer content\%blog_folder%\%blog_name%\index.md
-    echo content\%blog_folder%\%blog_name%\index.md
 )
 ENDLOCAL
