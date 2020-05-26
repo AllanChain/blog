7d1ecf6c643ca451d78b10433f425953e6e50336
Allan Chain <allanchain@pku.edu.cn>
2020-05-26 16:41:04 +0800

    Feature: markdown rendering

--- api.github.js
+++ api.github.js
@@ -55,8 +55,8 @@ const matchFirst = (pattern, text) => {
 }
 
 const parseBody = text => {
-  text = text.split('---', 2)[0]
-  const result = {}
+  const result = {};
+  [text, result.body] = text.split('---', 2)
   for (const key in patterns) {
     result[key] = matchFirst(patterns[key], text)
   }
@@ -69,7 +69,6 @@ module.exports = async () => {
   const posts = repo.issues.edges.map(edge => ({
     ...parseBody(edge.node.body),
     title: edge.node.title,
-    body: edge.node.body,
     labels: edge.node.labels.edges.map(edge => edge.node.name)
   }))
   const labels = repo.labels.edges.map(edge => edge.node)
--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,18 +1,22 @@
 <template>
-  <h1>{{ $page.post.title }}</h1>
+  <div>
+    <h1>{{ $page.post.title }}</h1>
+    <vue-markdown>{{ $page.post.body }}</vue-markdown>
+  </div>
 </template>
 
 <page-query>
-query($id: ID!) {
-  post(id: $id) {
-    title
+  query($id: ID!) {
+    post(id: $id) {
+      title
+      body
+    }
   }
-}
 </page-query>
 
 <script>
 export default {
-  created() {
+  created () {
     console.log(this.$page)
   }
 }
