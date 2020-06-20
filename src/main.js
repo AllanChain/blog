8de1d9834f7013345c1f87d252e7efe060797be2
Allan Chain <allanchain@pku.edu.cn>
2020-06-20 21:16:04 +0800

    refractor: move sass to one file

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-import './style/index.sass'
+// import 'vuetify/dist/vuetify.min.css'
+import './style.sass'
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { appOptions, router, head, isClient }) {
