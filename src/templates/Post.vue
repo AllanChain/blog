4cdf9d2c6f80cb0840dff7b49666ae10989ed600
Allan Chain <allanchain@pku.edu.cn>
2020-06-20 16:06:20 +0800

    feat(post): create time and tag

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,6 +1,19 @@
 <template>
   <Layout>
-    <h1>{{ $page.post.title }}</h1>
+    <div class="article-head text-center">
+      <h1 class="article-title">
+        {{ $page.post.title }}
+      </h1>
+      <div class="mb-2">
+        <PostTag
+          v-for="tag of $page.post.tag"
+          :key="tag.id"
+          :color="`#${tag.color}`"
+          :tag="tag"
+        />
+      </div>
+      <div>{{ formatTime($page.post.createdAt) }}</div>
+    </div>
     <article
       class="article-main markdown-body"
       v-html="$page.post.body"
@@ -11,22 +24,33 @@
 <page-query>
   query($id: ID!) {
     post(id: $id) {
+      id
       title
+      summary
       body
+      createdAt
+      image
+      tag {
+        id
+        color
+        path
+      }
     }
   }
 </page-query>
 
 <script>
+import { formatTime } from '@/utils'
+import PostTag from '@/components/PostTag'
+
 export default {
   metaInfo () {
     return {
       title: this.$page.post.title
     }
   },
-  created () {
-    console.log(this.$page)
-  }
+  components: { PostTag },
+  methods: { formatTime }
 }
 </script>
 
