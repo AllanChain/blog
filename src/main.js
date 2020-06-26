1b1baaac96ecfef6c6bd095e437657067b7d5167
Allan Chain <allanchain@pku.edu.cn>
2020-06-26 09:20:28 +0800

    feat: add vuex
    Use classic vue-cli main.js style

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import Vuetify from 'vuetify/lib'
+import store from '@/store'
+import vuetify from '@/plugins/vuetify'
 import './style.sass'
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { appOptions, router, head, isClient }) {
+  Object.assign(appOptions, { vuetify, store })
+
   head.link.push({
     rel: 'stylesheet',
     href: 'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css'
@@ -14,14 +17,6 @@ export default function (Vue, { appOptions, router, head, isClient }) {
   head.link.push({
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900'
-  })
-
-  Vue.use(Vuetify)
-
-  appOptions.vuetify = new Vuetify({
-    icons: {
-      iconfont: 'mdi'
-    }
   })
 
   // Set default layout as a global component
