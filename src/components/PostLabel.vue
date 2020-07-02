a8100ed934828690d98fd87c02c59ad7e1e43e6e
Allan Chain <allanchain@pku.edu.cn>
2020-07-02 10:19:38 +0800

    refractor(label): use data bind
    Instead of brute v-if

--- src/components/PostLabel.vue
+++ src/components/PostLabel.vue
@@ -1,36 +1,14 @@
 <template>
   <div class="ma-1 d-inline-block">
     <v-chip
-      v-if="label.type === 'tag'"
-      :color="`#${label.color}`"
-      :text-color="textColor"
-      class="font-weight-bold"
-      :to="label.path"
-      small
-    >
-      <v-icon left small>mdi-tag-outline</v-icon>
-      {{ label.name }}
-    </v-chip>
-    <v-chip
-      v-else-if="label.type === 'series'"
       :color="`#${label.color}`"
       class="font-weight-bold"
       :to="label.path"
-      small
-      outlined
+      v-bind="style"
     >
-      <v-icon left small>mdi-book</v-icon>
-      {{ label.name }}
-    </v-chip>
-    <v-chip
-      v-else-if="label.type === 'blog'"
-      :color="`#${label.color}`"
-      class="font-weight-bold"
-      :to="label.path"
-      small
-      label
-    >
-      <v-icon left small>mdi-archive</v-icon>
+      <v-icon left small>
+        {{ icon }}
+      </v-icon>
       {{ label.name }}
     </v-chip>
   </div>
@@ -38,6 +16,9 @@
 
 <script>
 import { isDarkColor } from '@/utils'
+
+const textColor = color => isDarkColor(color, 101) ? 'white' : 'black'
+
 export default {
   props: {
     label: {
@@ -45,11 +26,27 @@ export default {
       required: true
     }
   },
-  computed: {
-    textColor () {
-      if (isDarkColor(this.label.color, 101)) return 'white'
-      return 'black'
+  data () {
+    const data = {
+      style: {
+        small: true,
+        label: false,
+        outlined: false,
+        textColor: textColor(this.label.color)
+      },
+      icon: ''
     }
+    if (this.label.type === 'tag') {
+      data.icon = 'mdi-tag-outline'
+    } else if (this.label.type === 'series') {
+      data.icon = 'mdi-book'
+      data.style.textColor = undefined
+      data.style.outlined = true
+    } else if (this.label.type === 'blog') {
+      data.icon = 'mdi-archive'
+      data.style.label = true
+    }
+    return data
   }
 }
 </script>
