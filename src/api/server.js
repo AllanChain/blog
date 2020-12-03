572a031d186bad12740f8504e91c37ecda678d41
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2020-12-03 16:05:04 +0800

    fix: server data request error handling

--- src/api/server.js
+++ src/api/server.js
@@ -8,16 +8,26 @@ module.exports = async function serverData (variables) {
     resolve(__dirname, './data.gql'),
     { encoding: 'utf-8' }
   )
+  let resp
   variables = { ...config.gqlVar, ...variables }
 
-  const resp = await axios({
-    method: 'post',
-    url: 'https://api.github.com/graphql',
-    data: { query, variables },
-    headers: {
-      Authorization: `bearer ${process.env.GITHUB_TOKEN}`
+  try {
+    resp = await axios({
+      method: 'post',
+      url: 'https://api.github.com/graphql',
+      data: { query, variables },
+      headers: {
+        Authorization: `bearer ${process.env.GITHUB_TOKEN}`
+      }
+    })
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.data)
+    } else {
+      console.log('Error', error.message)
     }
-  })
+    throw error
+  }
   if (resp.data.error) {
     console.log(resp.data.errors)
     throw resp.data.errors[0].message
