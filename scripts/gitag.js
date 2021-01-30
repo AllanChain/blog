0693628256781173a062601a4dfe53e2fe62f4a0
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2021-01-30 21:55:20 +0800

    chore(release): v1.11.0-beta.0

---
+++ scripts/gitag.js
@@ -0,0 +1,6 @@
+const { execSync } = require('child_process')
+const { version } = require('../package.json')
+
+execSync('git add -A')
+execSync(`git commit -v -m "chore(release): v${version}"`)
+execSync(`git tag v${version}`)
