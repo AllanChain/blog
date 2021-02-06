7f28d9dfe95e2141fc5502e02b377f9eaf6eb835
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2021-02-06 16:13:21 +0800

    chore: sign tags

--- scripts/gitag.js
+++ scripts/gitag.js
@@ -3,4 +3,5 @@ const { version } = require('../package.json')
 
 execSync('git add -A')
 execSync(`git commit -v -m "chore(release): v${version}"`)
-execSync(`git tag v${version}`)
+execSync(`git tag v${version} -sm "${version}"`)
+execSync('git push --follow-tags')
