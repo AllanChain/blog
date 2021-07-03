ddebb405a50ef1e4cfcad6704c9fc1df726d933b
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2021-07-03 18:03:38 +0800

    chore: remove roboto font
    It's not of much use and sometimes blocking site load

--- src/main.js
+++ src/main.js
@@ -25,10 +25,6 @@ export default function (Vue, { appOptions, router, head, isClient }) {
     rel: 'stylesheet',
     href: 'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css'
   })
-  head.link.push({
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&display=swap'
-  })
 
   router.options.scrollBehavior = (to, from, savedPosition) => {
     const target = savedPosition ? savedPosition.y : 0
