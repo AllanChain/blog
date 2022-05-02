98c0faf2e4d5945e35ebfdf1a4f56aef399c59dc
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2022-05-02 09:42:02 +0800

    style: prettify

--- src/components/SvelteCounter.svelte
+++ src/components/SvelteCounter.svelte
@@ -1,17 +1,17 @@
 <script>
-  let count = 0;
+  let count = 0
 
   function add() {
-    count += 1;
+    count += 1
   }
 
   function subtract() {
-    count -= 1;
+    count -= 1
   }
 </script>
 
 <div id="svelte" class="counter">
   <button on:click={subtract}>-</button>
-  <pre>{ count }</pre>
+  <pre>{count}</pre>
   <button on:click={add}>+</button>
 </div>
