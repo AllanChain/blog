b4d005ef59c6c139fe79dfddbab98161bd9b4d60
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2022-07-12 08:09:27 +0800

    feat: experimental RSS

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,17 @@
+import rss from '@astrojs/rss'
+import dataPromise from '@data'
+
+export const get = async () => {
+  const { posts } = await dataPromise
+  return rss({
+    title: 'AC Dustbin',
+    description: 'A home for every piece of my dust.',
+    site: 'https://allanchain.github.io/blog/post/',
+    items: posts.map((post) => ({
+      link: post.slug,
+      title: post.title,
+      pubDate: post.createdAt,
+      description: post.summary,
+    })),
+  })
+}
