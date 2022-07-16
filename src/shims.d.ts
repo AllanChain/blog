abc0bdbd15b0ccfa90c3b498dc7c1468a1975677
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2022-07-16 16:54:25 +0800

    chore: fix some typing

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,33 +1,8 @@
+import * as astro from 'astro'
 import type { AttributifyAttributes } from '@unocss/preset-attributify'
 
 declare global {
   namespace astroHTML.JSX {
-    interface HTMLAttributes<T> extends AttributifyAttributes {}
-    export type Child =
-      | Node
-      | Node[]
-      | string
-      | number
-      | boolean
-      | null
-      | undefined
-      | unknown
-    export type Children = Child | Child[]
-
-    interface ElementChildrenAttribute {
-      // eslint-disable-next-line @typescript-eslint/ban-types
-      children: {}
-    }
-
-    interface IntrinsicAttributes
-      extends AstroBuiltinProps,
-        AstroBuiltinAttributes {
-      slot?: string
-      children?: Children
-      class?: string
-    }
-
-    type AstroBuiltinProps = import('astro').AstroBuiltinProps
-    type AstroBuiltinAttributes = import('astro').AstroBuiltinAttributes
+    interface HTMLAttributes extends AttributifyAttributes {}
   }
 }
