4789407b4fedd0d33e2325a932b0d3e5a9b71c83
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2024-08-01 18:59:21 +0800

    feat: fancy post card inspired by astro-theme-pure

--- astro.config.mjs
+++ astro.config.mjs
@@ -41,8 +41,6 @@ export default defineConfig({
           'dash-divider',
           'border-t-2 border-gray-400 dark:border-gray-600 border-t-dashed flex-grow h-0 mx-2',
         ],
-        ['card-container', 'flex flex-wrap items-center justify-evenly'],
-        ['card-item', 'm-1 w-xs grow-0.3'],
         [
           'theme-link',
           'text-current decoration-none hover:decoration-dashed hover:decoration-underline',
